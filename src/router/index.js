d14a4341a5fe80caea71c69b703d6d00ebe989d4
xiaoyu-c <1581523609@qq.com>
2021-08-07 14:22:32 +0800

    main

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Home = () => import('../views/home/Home.vue')
+const Category = () => import('../views/category/Category.vue')
+const Shopcart = () => import('../views/shopcart/Shopcart.vue')
+const Profile = () => import('../views/profile/Profile.vue')
+
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path:''
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/shopcart',
+    component: Shopcart
+  },
+  {
+    path: '/profile',
+    component: Profile
   },
 ]
 
