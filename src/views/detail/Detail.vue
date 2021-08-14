42d21cd0dd4953e3f4027613e3d42bab2c28f227
xiaoyu-c <1581523609@qq.com>
2021-08-14 16:51:10 +0800

    main

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,38 @@
 <template>
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :topImages="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar.vue'
+import DetailSwiper from './childComps/DetailSwiper.vue'
 
+import { getDetail } from '../../network/detail'
 
 export default {
   name: 'Detail',
   data () {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   components: {
     DetailNavBar,
+    DetailSwiper
   },
   created () {
+    // 保存传入的iid
     this.iid = this.$route.query.iid
+
+    // 根据iid请求详细数据
+    getDetail(this.iid).then(res => {
+      console.log(res);
+      // 1、获取顶部轮播数据
+      this.topImages= res.result.itemInfo.topImages
+    })
   },
 }
 </script>
---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+    <swiper class="detail-swiper">
+      <swiper-item v-for="(item,index) in topImages" :key="index">
+        <img :src="item" alt="">
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from '../../../components/common/swiper/index';
+
+export default {
+  components: {
+    Swiper,
+    SwiperItem
+  },
+props: {
+  topImages: {
+    type: Array,
+    default: []
+  },
+},
+}
+</script>
+
+<style scoped>
+.detail-swiper{
+  height: 300px;
+  overflow: hidden;
+}
+</style>
