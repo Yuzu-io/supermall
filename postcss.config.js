93a99539a318a2757dac15b76c076aa716e00d32
xiaoyu-c <1581523609@qq.com>
2021-08-21 09:23:17 +0800

    main

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//   plugins:{
+//     autoprefixer:{},
+//     "postcss-px-to-viewport":{
+//       viewportWidth:375,
+//       viewportHeight:667,
+//       unitPrecision:5,
+//       viewportUnit:'vw',
+//       selectorBlackList:['ignore','tab-bar','tab-bar-item'],//指出不需要转换的类
+//       minPixelValue:1,
+//       mediaQuery:false
+//     },
+//   }
+// }
